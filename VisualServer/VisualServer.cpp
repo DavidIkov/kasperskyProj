1e2caec718b664b2913e2d48df12125f56687d0b
DavidIkov <postnikovdavid@yandex.ru>
2025-04-14 14:56:53 +0300

    yy

--- VisualServer/VisualServer.cpp
+++ VisualServer/VisualServer.cpp
@@ -1,12 +1,18 @@
 #include"VisualServer.hpp"
+#include"httpparser/include/response.h"
+#include"httpparser/include/httpresponseparser.h"
+
 
 size_t CVisualServer::CClientSocketForVisualServer::OnRead(size_t bytesReserved, uint8_t* buf, size_t newBytes) {
-    size_t lastZero = 0;
-    for (size_t i = bytesReserved;i < bytesReserved + newBytes;i++)
-        if (buf[i] == 0) {
-            unsigned bytes = i - lastZero;
-            printf("server received a message: \"%.*s\"\n", (int)bytes, buf + lastZero);
-            lastZero = i + 1;
-        }
-    return bytesReserved + newBytes - lastZero;
+    return StandartParser(bytesReserved, buf, newBytes, [this](uint8_t* buf, size_t len) {
+        httpparser::Response response;
+        httpparser::HttpResponseParser parser;
+
+        httpparser::HttpResponseParser::ParseResult res =
+            parser.parse(response, (const char*)buf, (const char*)(buf + len));
+        if (res == httpparser::HttpResponseParser::ParsingCompleted)
+            printf("server received a message \"%.*s\"\n", (int)response.content.size(), response.content.data());
+        else
+            fprintf(stderr, "cant parse http, it contains error\n");
+        });
 }
